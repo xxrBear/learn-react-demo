77c54ac8472772c85aab1f8aa44e56b1b66593f4
bear <xxrBear@gmail.com>
2025-02-20 20:36:41 +0800

    增加项目

--- my-react/src/App.js
+++ my-react/src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+export default function App() {
+  function handleClick() {
+    alert("你点我干嘛！");
+  }
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}>点我</button>
     </div>
   );
 }
-
-export default App;
